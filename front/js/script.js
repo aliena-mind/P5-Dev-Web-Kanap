08a8d0113b4e09076c680afcf5b65d83f40e78a1
aliena-mind <110843653+aliena-mind@users.noreply.github.com>
2022-11-07 10:50:56 -0400

    Ajout des produits sur l'index en js
    - Mise en place d'un fetch
    - Mise en place d'une boucle for pour extraire les données en fonction de l'index de l'objet (création des éléments, définition de la hiérarchie des éléments, ajout classe/nom produit, ajout classe/description produit, ajout des liens)

---
+++ front/js/script.js
@@ -0,0 +1,40 @@
+fetch('http://localhost:3000/api/products/')    // lien vers API, requete GET via fetch
+    .then(function(result) {
+        if (result.ok) {                        // si result ok
+        return result.json();                   // envoie la réponse au format JSON
+        }
+    })
+    .then(function(data) {
+        console.log(data)
+
+        // boucle for pour ajouter les résultats en fonction du nombre de 'data'
+        for (let i = 0; i < data.length; i++) {  
+            // création des éléments 
+            const lien = document.createElement('a');                   // création d'un lien
+            const article = document.createElement('article');          // création d'un article
+            const imageArticle = document.createElement('img');         // création d'une image
+            const titreArticle = document.createElement('h3');          // création d'un titre
+            const paragrapheArticle = document.createElement('p');      // crétion d'un paragraphe
+
+            // définition de la hiérarchie des éléments
+            lien.appendChild(article);                                  // 'article' enfant de 'lien'
+            article.appendChild(imageArticle);                          // 'imageArticle' enfant de 'article'
+            article.appendChild(titreArticle);                          // 'titreArticle' enfant de 'article'
+            article.appendChild(paragrapheArticle);                     // 'paragrapheArticle' enfant de 'article'
+
+            // ajout classe/nom produit
+            titreArticle.className = "nomProduit";                      // définit le nom de classe de 'titreArticle'
+            titreArticle.innerHTML = data[i].name;                      // 'data[i].name' = nom de l'élément data d'index 'i'
+
+            // ajout classe/description produit
+            paragrapheArticle.className = "descriptionProduit";         // définit le nom de classe de 'paragrapheArticle'
+            paragrapheArticle.innerHTML = data [i].description;         // 'data [i].description' = description de l'élément data d'index 'i'
+
+            // ajout des liens
+            lien.href = './product.html?id=${data[i]._id}';             // définit l'attribut 'href' du lien 
+            imageArticle.src = data[i].imageUrl;                        // définit l'attribut 'src' de l'image
+            imageArticle.alt = data[i].altText;                         // définit l'attribut 'alt' de l'image
+
+            document.querySelector("#items").appendChild(lien);          // 'lien' enfant de la class 'items' déja existante dans le code html
+        }
+    })
