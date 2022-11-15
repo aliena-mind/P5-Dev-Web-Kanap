36a6f2c29973a4c5d7b42ef31a0ba9996fe5f15f
aliena-mind <110843653+aliena-mind@users.noreply.github.com>
2022-11-14 21:18:23 -0400

    Modification sommaires
    - Ajout quantité totale
    - Ajout prix total
    - Ajout des attributs

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,4 +1,4 @@
-//////////////////////// Sauvegarde & récupération du panier /////////////////
+//////////////////////// gestion du panier /////////////////////
 
 // sauvegarde du panier
 function sauvegarderPanier(panier) {
@@ -28,7 +28,14 @@ fetch('http://localhost:3000/api/products/')    // lien vers API, requete GET vi
     .then(function(data) {
         let panier = recupererPanier();
 
+        let prixTotal = 0;                              // prix total du panier
+        let quantiteTotale = 0;                         // quantité totale du panier
+
         for (i = 0; i<panier.length; i++) {             // pour chaque élément du panier :
+
+            // calcul de la quantité total
+            quantiteTotale += panier[i].quantity;       
+            document.querySelector("#totalQuantity").innerHTML = quantiteTotale; 
 
             for (j=0; j<data.length; j++) {             // pour chaque élément de l'api :
                 
@@ -51,7 +58,7 @@ fetch('http://localhost:3000/api/products/')    // lien vers API, requete GET vi
                     const inputQuantite = document.createElement('input');          // création d'une div
 
                     const divSupprimer = document.createElement('div');             // création d'une div
-                    const Supprimer = document.createElement('p');                  // création d'un paragraphe
+                    const supprimer = document.createElement('p');                  // création d'un paragraphe
 
                     // définition de la hiérarchie des éléments
                     article.appendChild(divImage);               // 'divImage' enfant de 'article'
@@ -69,7 +76,7 @@ fetch('http://localhost:3000/api/products/')    // lien vers API, requete GET vi
                     divQuantite.appendChild(inputQuantite);      // 'inputQuantite' enfant de 'divQuantite'
 
                     parametres.appendChild(divSupprimer);        // 'divSupprimer' enfant de 'parametres'
-                    divSupprimer.appendChild(Supprimer);         // 'quantite' enfant de 'divSupprimer'
+                    divSupprimer.appendChild(supprimer);         // 'supprimer' enfant de 'divSupprimer'
 
                     // ajout de la classe des éléments
                     article.className = "cart__item";                           // définit 'article'
@@ -83,18 +90,30 @@ fetch('http://localhost:3000/api/products/')    // lien vers API, requete GET vi
                     inputQuantite.className = "itemQuantity";                   // définit 'inputQuantite'
 
                     divSupprimer.className = "cart__item__content__settings__delete"; // définit  'divSupprimer'
-                    Supprimer.className = "deleteItem";                         // définit 'Supprimer'
+                    supprimer.className = "deleteItem";                         // définit 'Supprimer'
 
                     // ajout des attributs
-                    article.setAttribute('data-id', panier[i].id);              // 'data-id'
-                    article.setAttribute('data-couleur', panier[i].couleur);    // 'data-couleur' 
-                    image.src = data[j].imageUrl;                               // 'src' de l'image
-                    image.alt = data[j].imageUrl;                               // 'alt' de l'image
+                    article.setAttribute('dataId', panier[i].id);                // 'dataId'
+                    article.setAttribute('dataCouleur', panier[i].couleur);      // 'dataCouleur' 
+                    inputQuantite.setAttribute('type', 'number');                // 'type'
+                    inputQuantite.setAttribute('name', 'itemQuantity');          // 'name'
+                    inputQuantite.setAttribute('min', '1');                      // 'min'
+                    inputQuantite.setAttribute('max', '100');                    // 'max'
+                    inputQuantite.setAttribute('value', '');                     // 'value'
+                    supprimer.setAttribute('dataId', panier[i].id);              // 'dataId'
+                    image.src = data[j].imageUrl;                                // 'src' de l'image
+                    image.alt = data[j].imageUrl;                                // 'alt' de l'image
+
+                    // calcul du prix total
+                    prixTotal += data[j].price;         
+                    document.querySelector("#totalPrice").innerHTML = prixTotal;
 
                     // ajout inner HTML
-                    couleur.innerHTML = article.getAttribute('data-couleur');   // affiche la couleur
+                    couleur.innerHTML = article.getAttribute('dataCouleur');    // affiche la couleur
                     nom.innerHTML = data[j].name;                               // affiche le nom
                     prix.innerHTML = data[j].price + ' €';                      // affiche le prix
+                    quantite.innerHTML = panier[i].quantity;                    // affiche la quantité
+                    supprimer.innerHTML = 'Supprimer';                          // bouton supprimer
 
                     // 'article' enfant de la class 'cart__items' déja existante dans le code html
                     document.querySelector("#cart__items").appendChild(article);
@@ -102,3 +121,4 @@ fetch('http://localhost:3000/api/products/')    // lien vers API, requete GET vi
             }
         }
     })
+
